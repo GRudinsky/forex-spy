633b4a694bc7990557ba7ae7bde3f369a9e76082
Giedrius Rudzianskas <g.rudzianskas@gmail.com>
2021-08-02 23:26:52 +0100

    add services test

--- src/utils/services/services.spec.js
+++ src/utils/services/services.spec.js
@@ -1,43 +1,22 @@
-import { list } from './list';
+import { getCurrencies } from './services';
+import { URL_OXR } from '../constants';
 
 const fetch = global.fetch;
 
-const products = [
-  {
-    productId: 100024698,
-    name: 'Rose Gold Shimmer Clutch Bag',
-    description:
-      '• Shimmer finish\n• Pleat design\n• Magnetic closure\n• Inner pocket\n• Detachable chain strap',
-    price: 9.09,
-    priceWas: 12.99,
-    available: 'TRUE',
-    quantity: 96,
-    lowOnStock: 'FALSE',
-    promotionBadge: '30% OFF',
-    imageUrl: 'https://i8.amplience.net/i/Quiz/00100024698_XM?w=1024'
-  },
-  {
-    productId: 100024699,
-    name: 'Blue Jewel Clutch Bag',
-    description:
-      '• Jewel finish\n• Silver colour frame\n• Clip closure\n• Detachable chain strap\n• Synthetic',
-    price: 9.99,
-    priceWas: 18.99,
-    available: 'TRUE',
-    quantity: 3,
-    lowOnStock: 'FALSE',
-    promotionBadge: '47% OFF',
-    imageUrl: 'https://i8.amplience.net/i/Quiz/00100024699_XM?w=1024'
+const currencies = {
+  rates: {
+    AED: 1,
+    GBP: 2
   }
-];
+};
 
-describe('list', () => {
-  it('should return products in the response', async () => {
-    fetch.mockOnce(JSON.stringify(products));
+describe('getCurrencies', () => {
+  it('should return getCurrencies in the response', async () => {
+    fetch.mockOnce(JSON.stringify(currencies));
 
-    await list('someUrl').then((res) => {
-      expect(fetch).toHaveBeenCalledWith('someUrl', { method: 'GET' });
-      expect(res).toEqual(products);
+    await getCurrencies().then((res) => {
+      expect(fetch).toHaveBeenCalledWith(URL_OXR, { method: 'GET' });
+      expect(res).toEqual(currencies);
     });
   });
 
@@ -45,7 +24,7 @@ describe('list', () => {
     const errorResponse = new Error('Some Error');
     fetch.mockRejectOnce(errorResponse);
 
-    await list('someUrl').catch((e) => {
+    await getCurrencies().catch((e) => {
       expect(e.message).toBe('Some Error');
     });
   });
